3b328b64fe6ac6cec243c4559b09a618ef27240b
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2024-02-12 23:22:12 +0545

    added default config

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
-import config from './config/config.json';
+
 // let config;
 
 // fetching not working for iOS
@@ -19,13 +19,13 @@ import config from './config/config.json';
 // });
 
 firebase.initializeApp({
-  apiKey: config.firebaseConfig.apiKey,
-  authDomain: config.firebaseConfig.authDomain,
-  databaseURL: config.firebaseConfig.databaseURL,
-  projectId: config.firebaseConfig.projectId,
-  storageBucket: config.firebaseConfig.storageBucket,
-  messagingSenderId: config.firebaseConfig.messagingSenderId,
-  appId: config.firebaseConfig.appId
+  apiKey: "AIzaSyBu2emmod70NGOcDid_q81x3gP6FAW-Ro8",
+  authDomain: "push-notify-b8795.firebaseapp.com",
+  databaseURL: "https://push-notify-b8795-default-rtdb.firebaseio.com",
+  projectId: "push-notify-b8795",
+  storageBucket: "push-notify-b8795.appspot.com",
+  messagingSenderId: "131374839398",
+  appId: "1:131374839398:web:3fa637d133a5407c65dfa7"
 });
 
 if (firebase.messaging.isSupported()) {
